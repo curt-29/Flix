b3bbad999440b053ad34d8c77f293b2926b33aca
curt <32206277+curt-29@users.noreply.github.com>
2021-02-07 15:56:39 -0500

    Flix part 2

--- app/src/main/java/adapters/MovieAdapter.java
+++ app/src/main/java/adapters/MovieAdapter.java
@@ -1,20 +1,26 @@
 package adapters;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 
+import org.parceler.Parcels;
+
 import java.util.List;
 
+import codepath.AndroidUniversity.DetailActivity;
 import codepath.AndroidUniversity.Models.Movie;
 import codepath.AndroidUniversity.R;
 
@@ -51,7 +57,7 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
-
+        RelativeLayout container ;
         TextView tvTitle;
         TextView tvOverview;
         ImageView ivPoster;
@@ -61,12 +67,25 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
             tvTitle = itemView.findViewById(R.id.tvTitle);
             tvOverview = itemView.findViewById(R.id.tvOverview);
             ivPoster = itemView.findViewById(R.id.ivPoster);
+            container = itemView.findViewById(R.id.container);
         }
 
         public void bind(Movie movies) {
             tvTitle.setText(movies.getTitle());
             tvOverview.setText(movies.getOverview());
             Glide.with(context).load(movies.getPosterPath()).into(ivPoster);
+
+            //1.Register click listener on the whole row
+            //2. Navigate to a new activity on tap
+            container.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent i = new Intent(context, DetailActivity.class);
+                    i.putExtra("movie", Parcels.wrap(movies));
+                    context.startActivity(i);
+                }
+            });
+
+            }
         }
     }
-}
--- app/src/main/java/codepath/AndroidUniversity/Models/Movie.java
+++ app/src/main/java/codepath/AndroidUniversity/Models/Movie.java
@@ -3,19 +3,30 @@ package codepath.AndroidUniversity.Models;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.parceler.Parcel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Parcel
 public class Movie {
+
+    int movieId;
     String posterPath;
     String title;
     String overview;
+    double rating;
 
+    //empty contructor needed from parcel library
+    public Movie() {
+
+    }
     public Movie(JSONObject jsonObject) throws JSONException {
         posterPath = jsonObject.getString("poster_path");
         title = jsonObject.getString("title");
         overview = jsonObject.getString("overview");
+        rating = jsonObject.getDouble("vote_average");
+        movieId = jsonObject.getInt("id");
     }
 
     public static List<Movie> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
@@ -30,11 +41,19 @@ public class Movie {
         return String.format("https://image.tmdb.org/t/p/w342/%s",posterPath);
     }
 
-    public String getTitle() {
+    public String  getTitle() {
         return title;
     }
 
     public String getOverview() {
         return overview;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public int getMovieId() {
+        return movieId;
     }
 }
